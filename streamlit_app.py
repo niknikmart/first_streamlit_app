d4a2247928eaa3d1d58d3b9d127603e127e4e090
martdoulbenik <martdoulbenik@gmail.com>
2022-07-03 16:46:02 +0300

    Interactive filter added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
+from email import message_from_binary_file
 import streamlit
+import pandas
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -7,3 +9,11 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_selected = streamlit.multiselect('Pick some fruits: ', list(my_fruit_list.index))
+if fruits_selected:
+    fruits_to_show = my_fruit_list.loc[fruits_selected]
+else:
+    fruits_to_show = my_fruit_list
+streamlit.dataframe(fruits_to_show)
